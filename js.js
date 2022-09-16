1f724f8ed28ace1b6f9a03b0553bc6ac2370d73a
ludovic-estival <95869465+ludovic-estival@users.noreply.github.com>
2022-09-16 13:37:48 +0200

    début implémentation calculs

--- js.js
+++ js.js
@@ -1,56 +1,125 @@
 function load(){
-    /*document.getElementById("0").addEventListener('click',ajouter,false);
-    document.getElementById("1").addEventListener('click',ajouter,false);
-    document.getElementById("2").addEventListener('click',ajouter,false);
-    document.getElementById("3").addEventListener('click',ajouter,false);
-    document.getElementById("4").addEventListener('click',ajouter,false);
-    document.getElementById("5").addEventListener('click',ajouter,false);
-    document.getElementById("6").addEventListener('click',ajouter,false);
-    document.getElementById("7").addEventListener('click',ajouter,false);
-    document.getElementById("8").addEventListener('click',ajouter,false);
-    document.getElementById("9").addEventListener('click',ajouter,false);
-    document.getElementById("divi").addEventListener('click',ajouter,false);
-    document.getElementById("multi").addEventListener('click',ajouter,false);
-    document.getElementById("addi").addEventListener('click',ajouter,false);
-    document.getElementById("soust").addEventListener('click',ajouter,false);
-    document.getElementById("negate").addEventListener('click',ajouter,false);
-    document.getElementById("egal").addEventListener('click',ajouter,false);*/
     document.getElementById("egal").addEventListener('click',calcul,false);
+    activerOperations(false);
 }
 
+var nb1;
+var nb2;
+var result;
 
 function ajouter(valeur){
-    document.getElementById("resultat").innerHTML = valeur;
-}
-
-function afficher(){
-    
+    if(typeof nb1 === 'undefined'){
+        nb1 = valeur;
+        activerOperations(true);
+        activerNombres(false);
+        result = valeur;
+        afficher(result);
+    }
+    else{
+        nb2 = valeur;
+        activerNombres(false);
+        result += valeur;
+        afficher(result);
+    }
 }
 
 function operation(type){
+    activerNombres(true);
+    activerOperations(false);
+    result += type;
+    afficher(result);
+
     switch(type){
-        case('division'):
+        case('/'):
+        calcul(nb1/nb2);
         break;
 
-        case('multiplication'):
+        case('*'):
+        calcul(nb1*nb2);
         break;
 
-        case('addition'):
+        case('+'):
+        calcul(nb1+nb2);
         break;
 
-        case('soustraction'):
+        case('-'):
+        calcul(nb1-nb2);
         break;
 
-        case('negatif'):
+        case('neg'):
+        calcul(nb1/nb2);
+        break;
+
+        case(','):
+        calcul(nb1/nb2);
         break;
 
         default:
     }
 }
 
-function calcul(){
-
+function calcul(resultat){
+    afficher(resultat);
 }
 
 // POSSIBILITE 1 créer un tableau avec le calcul final, 1 calcul à la fois, 2 nombres + opération, vide le tab à la fin
 // POSSIBILITE 2 stocker les valeurs dans deux variables différentes et faire le calcul correspondant
+
+
+function afficher(resultat){
+    document.getElementById("resultat").innerHTML = resultat;
+}
+
+function reinitialiser(){
+    document.getElementById("resultat").innerHTML = "";
+}
+
+function activerOperations(valeur){
+    if(valeur == true){
+        document.getElementById("divi").disabled = false;
+        document.getElementById("multi").disabled = false;
+        document.getElementById("addi").disabled = false;
+        document.getElementById("soust").disabled = false;
+        document.getElementById("negate").disabled = false;
+        document.getElementById("deci").disabled = false;
+        //voir égal
+    }
+    
+    if(valeur == false){
+        document.getElementById("divi").disabled = true;
+        document.getElementById("multi").disabled = true;
+        document.getElementById("addi").disabled = true;
+        document.getElementById("soust").disabled = true;
+        document.getElementById("negate").disabled = true;
+        document.getElementById("deci").disabled = true;
+    }
+}
+
+function activerNombres(valeur){
+    if(valeur == true){
+        document.getElementById("0").disabled = false;
+        document.getElementById("1").disabled = false;
+        document.getElementById("2").disabled = false;
+        document.getElementById("3").disabled = false;
+        document.getElementById("4").disabled = false;
+        document.getElementById("5").disabled = false;
+        document.getElementById("6").disabled = false;
+        document.getElementById("7").disabled = false;
+        document.getElementById("8").disabled = false;
+        document.getElementById("9").disabled = false;
+    }
+    
+    if(valeur == false){
+        document.getElementById("0").disabled = true;
+        document.getElementById("1").disabled = true;
+        document.getElementById("2").disabled = true;
+        document.getElementById("3").disabled = true;
+        document.getElementById("4").disabled = true;
+        document.getElementById("5").disabled = true;
+        document.getElementById("6").disabled = true;
+        document.getElementById("7").disabled = true;
+        document.getElementById("8").disabled = true;
+        document.getElementById("9").disabled = true;
+    }
+
+}
