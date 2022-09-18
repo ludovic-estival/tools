577a08db8cec8015521d8501655b97b6b6e07ec0
ludovic-estival <95869465+ludovic-estival@users.noreply.github.com>
2022-09-18 09:01:14 +0200

    Implémentation timer et chronomètre

--- js-timer.js
+++ js-timer.js
@@ -3,12 +3,7 @@ document.addEventListener("DOMContentLoaded", load);
 function load(){
     document.getElementById("timerB").addEventListener('click',lancerTimer,false);
     document.getElementById("chronoB").addEventListener('click',lancerChrono,false);
-    //document.getElementById("deci").addEventListener('click',decimal,false);
-    //document.getElementById("egal").disabled = true;
-    //activerOperations(false);
 }
-
-//1h = 3600s 
 
 //l'état permet de changer l'action du bouton: lancer ou arrêter
 var etatTimer = false;
@@ -21,51 +16,82 @@ var chronoAff = "";
 var dureeTimer;
 var uniteTimer;
 
-function lancerTimer(){
-    dureeTimer = document.getElementById("timerDur").value;
-    uniteTimer = document.querySelector('input[name="unite"]:checked').value;   //h, m ou s
-    
-    switch(uniteTimer){
-        case('h'):
-        timerAff = dureeTimer + ":00:" + "00";
-        majTimer(timerAff);
-        break;
-        
-        case('m'):
-        timerAff =  dureeTimer + "00";
-        majTimer(timerAff);
-        break;
-        
-        case('s'):
-        timerAff = dureeTimer;
-        majTimer(timerAff);
-        break;
-        default:
+//variables pour le chronomètre
+var dureeChrono;
+var sec = 0;
+var min = 0;
+var h = 0;
+
+//intervalle compte à rebours
+var interv = setInterval(function () {
+    if(etatTimer && dureeTimer >= 0){
+        document.getElementById("timer").innerHTML = dureeTimer-- + " secondes restantes"   
     }
-    
-    var secondes = 0;
-    var minutes = 0;
-    var heures = 0;
-    
-    var dureeSecondes = dureeTimer * 60;
-    
-   const interv = setInterval(function () {
-        if(dureeTimer<=0){
-            clearInterval(interv);
+}, 1000);
+
+//intervalle chronomètre
+var interC = setInterval(function () {
+    if(etatChrono){
+        sec++;
+        if(sec >= 60){
+            min++;
+            sec = 0;
         }
-        document.getElementById("timer").innerHTML = dureeTimer--;
-    }, 1000);
+
+        if(min >= 60){
+            h++;
+            min = 0;
+        }
+
+        document.getElementById("chrono").innerHTML = h + ":" + min + ":" + sec;
+    }
+}, 1000);
+
+function lancerTimer(){
+    var interv;
     
+    //activation du timer pour la première fois
+    if(!etatTimer){
+        dureeTimer = document.getElementById("timerDur").value;
+        uniteTimer = document.querySelector('input[name="unite"]:checked').value;   //h, m ou s
+        
+        if(uniteTimer == "h"){
+            dureeTimer *= 3600;
+        }
+        if(uniteTimer == "m"){
+            dureeTimer *= 60;
+        }
+        
+        document.getElementById("timerB").innerHTML = "Arrêter le timer";
+        etatTimer = true;
+        
+    }
+    else{
+        //arrêter le timer
+        clearInterval(interv);
+        document.getElementById("timerB").innerHTML = "Activer le compte à rebours";
+        document.getElementById("timer").innerHTML = "";
+        etatTimer = false;
+    }  
 }
 
 function lancerChrono(){
+    var interv;
     
+    //activation du chrono pour la première fois
+    if(!etatChrono){
+        etatChrono = true; 
+        document.getElementById("chronoB").innerHTML = "Arrêter le chronomètre";
+    }
+    else{
+        //arrêter le chrono
+        clearInterval(interC);
+        document.getElementById("chronoB").innerHTML = "Activer le compte à rebours";
+        document.getElementById("chrono").innerHTML = "";
+        etatChrono = false;
+    }  
 }
 
-function majTimer(){
-    document.getElementById("timer").innerHTML += dureeTimer--;
-}
-
-function majChrono(valeur){
-    document.getElementById("chrono").innerHTML = valeur;
+function arret(interval){
+    
 }
